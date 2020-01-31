797fb0022e6c77cbc558b959c2ce3fb2f19ee449
Ritik Bhardwaj <ritik.bhardwaj889@gmail.com>
2020-01-31 20:21:23 +0530

    set up ejs files and basic routing

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
-var express = require('express');
-var path = require('path');
+const express = require('express');
+const path = require('path');
+const app = express();
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// express middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+//route
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render("login");
+});
+
+router.get('/quiz',function(req,res){
+  let obj = {
+    username: "Kinglolo",
+    roll_number: 181000
+  }
+  res.render('quiz',{username: obj.username, roll_number: obj.roll_number});
+});
+
+router.post('/quiz', function (req, res) {
+  console.log("Post request!");
+  res.render
 });
 
 module.exports = router;
