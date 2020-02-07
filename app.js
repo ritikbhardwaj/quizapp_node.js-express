1d3d09412edbfc792b36d3733c2e1635c810d429
Ritik Bhardwaj <ritik.bhardwaj889@gmail.com>
2020-02-07 10:16:35 +0530

    remove semicolons

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const path = require('path');
-const app = express();
-const indexRouter = require('./routes/index');
+const express = require('express')
+const path = require('path')
+const app = express()
+const indexRouter = require('./routes/index')
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 // express middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
 
 //route
-app.use('/', indexRouter);
+app.use('/', indexRouter)
 
-module.exports = app;
+module.exports = app
--- database.js
+++ database.js
@@ -1,7 +1,7 @@
-const config = require("./config/config.json");
-const Sequelize = require("sequelize");
-const questionModel = require("./models/questions");
-const resultModel = require("./models/result");
+const config = require("./config/config.json")
+const Sequelize = require("sequelize")
+const questionModel = require("./models/questions")
+const resultModel = require("./models/result")
 
 const sequelize = new Sequelize({
     username: config.username,
@@ -9,18 +9,18 @@ const sequelize = new Sequelize({
     host: config.host,
     dialect: config.dialect,
     database: config.database
-});
+})
 
 // Connect all the models/tables in the database to a db object,
 //so everything is accessible via one object
-const db ={}; 
+const db ={} 
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
+db.sequelize = sequelize
+db.Sequelize = Sequelize
 
 //Models/tables
-db.Ques = require("./models/questions")(sequelize,Sequelize);
-db.Result = require("./models/result")(sequelize, Sequelize);
+db.Ques = require("./models/questions")(sequelize,Sequelize)
+db.Result = require("./models/result")(sequelize, Sequelize)
 
 //exposing the connection to other files
-module.exports = db;
+module.exports = db
