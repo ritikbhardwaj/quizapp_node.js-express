4362cfced40d6b4f265d859f6d084527aa147ab6
Ritik Bhardwaj <ritik.bhardwaj889@gmail.com>
2020-02-07 14:12:58 +0530

    resolve more conflicts with the merge conflicts

--- database.js
+++ database.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 const config = require("./config/config.json")
 const Sequelize = require("sequelize")
 const questionModel = require("./models/questions")
 const resultModel = require("./models/result")
-=======
-const config = require("./config/config.json");
-const Sequelize = require("sequelize");
-const questionModel = require("./models/questions");
-const resultModel = require("./models/result");
->>>>>>> b4bb679dfaf5afb6c347870c035c0bdbd0ca6ae1
 
 const sequelize = new Sequelize({
     username: config.username,
@@ -16,7 +9,6 @@ const sequelize = new Sequelize({
     host: config.host,
     dialect: config.dialect,
     database: config.database
-<<<<<<< HEAD
 })
 
 // Connect all the models/tables in the database to a db object,
@@ -32,20 +24,3 @@ db.Result = require("./models/result")(sequelize, Sequelize)
 
 //exposing the connection to other files
 module.exports = db
-=======
-});
-
-// Connect all the models/tables in the database to a db object,
-//so everything is accessible via one object
-const db ={}; 
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-//Models/tables
-db.Ques = require("./models/questions")(sequelize,Sequelize);
-db.Result = require("./models/result")(sequelize, Sequelize);
-
-//exposing the connection to other files
-module.exports = db;
->>>>>>> b4bb679dfaf5afb6c347870c035c0bdbd0ca6ae1
